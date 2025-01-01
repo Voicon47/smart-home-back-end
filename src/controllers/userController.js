2b6a670d1c8ec6c4ca8a46f468f349b5a557dd46
ELephant47 <vandinhdung2003@gmail.com>
2025-01-01 21:42:12 +0700

    #deploy update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,13 @@
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
     try {
         console.log(req.body)
-        res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new user'})
+        throw new ApiError(StatusCodes.BAD_REQUEST,'Elephant47 test error')
+        //res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new user'})
     } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            errors: new Error(error).message
-        })
+        next(error)
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -4,20 +4,35 @@ import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 import exitHook from 'async-exit-hook'
 import { env } from './config/environment'
 import { APIs_V1 } from './routes/v1/index'
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
+  // const hostname = 'localhost'
+  // const port = 8017
   console.log('2.Connected to MongoDB Cloud Atlas')
 
+  //Enable req.body json data
   app.use(express.json())
+  //Use APIs v1
   app.use('/v1',APIs_V1)
+  //Middleware
+  app.use(errorHandlingMiddleware)
+  // Production enviroment
+  if(env.BUILD_MODE === 'production'){
+    app.listen(process.env.PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`3.Prodcution: Hello ${env.AUTHOR}, I am running at Port:${ process.env.PORT }/`)
+    })
+  } else {
+    ///Local dev enviroment
+    app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
+      // eslint-disable-next-line no-console
+      console.log(`3.Local Dev: Hello ${env.AUTHOR}, I am running at ${ process.env.LOCAL_DEV_APP_HOST }:${ process.env.LOCAL_DEV_APP_PORT }/`)
+    })
+  }
+
   
-  app.listen(port, hostname, () => {
-    // eslint-disable-next-line no-console
-    console.log(`3.Hello ${env.AUTHOR}, I am running at ${ hostname }:${ port }/`)
-  })
 
   exitHook(() => {
     console.log(`Exit app`)
