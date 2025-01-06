03c19ef10e1216f3ef6adba593638063cf267646
ELephant47 <vandinhdung2003@gmail.com>
2025-01-06 15:48:44 +0700

    #2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const WHITELIST_DOMAINS = [
     'http://localhost:5173',
-    // 'https://smart-home-front-end-4enj.vercel.app',
+    'https://smart-home-front-end-4enj.vercel.app',
     
 ]
