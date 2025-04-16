1df780cac89173994df054a3c6d34f0567cc1f83
Elephant47 <vandinhdung2003@gmail.com>
2025-04-16 16:32:43 +0700

    Add firebase-service-account.json to .gitignore

--- src/config/environment.js
+++ src/config/environment.js
@@ -12,5 +12,10 @@ export const env = {
 
     BUILD_MODE: process.env.BUILD_MODE,
 
-    AUTHOR: process.env.AUTHOR
+    AUTHOR: process.env.AUTHOR,
+
+    //Firebase Admin
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,15 @@
 // /src/config/firebase.js
 import admin from 'firebase-admin';
-import serviceAccount from './firebase-service-account.json'; // download from Firebase Console
+// import serviceAccount from './firebase-service-account.json'; // download from Firebase Console
+import { env } from './environment';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    "projectId": env.FIREBASE_PROJECT_ID,
+    "private_key": env.FIREBASE_PRIVATE_KEY,
+    "client_email": env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: ""
 });
 
 export default admin;
