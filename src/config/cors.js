86fb9b9f1d055d738a7f86d01e6c1bbbcab5541e
Elephant47 <vandinhdung2003@gmail.com>
2026-02-02 11:54:15 +0700

    Update

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,7 +6,7 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
 export const corsOptions = {
   origin: function (origin, callback) {
-    console.log("Cors origin: ",origin)
+    console.log("Cors origin: ", origin)
     // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
     // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
     // Update mới: Ở video số 75 trong chuỗi MERN Stack PRO khi chúng ta deploy dự án lên một Server Production thì sẽ sửa lại đoạn này thêm một chút nữa để phù hợp với từng môi trường production hoặc dev nhé. Học với mình thì các bạn cứ yên tâm về sự chỉn chu chuẩn chỉnh nhé :D
@@ -15,12 +15,12 @@ export const corsOptions = {
     }
 
     // Kiểm tra xem origin có phải là domain được chấp nhận hay không
-    if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
-    }
+    // if (WHITELIST_DOMAINS.includes(origin)) {
+    return callback(null, true)
+    // }
 
     // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+    // return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
   },
 
   // Some legacy browsers (IE11, various SmartTVs) choke on 204
